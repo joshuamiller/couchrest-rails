da0f16ddfb8a3840ff2db3e756251cf9d81a6800
Joshua Miller <josh@joshinharrisburg.com>
2009-08-02 21:06:45 -0400

    Disabled rspec startup for compatibility with test::unit projects.  Quick hack for now; will optionalize later.

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'couch_rest_rails'
 require 'validatable'
-require 'spec/rails/matchers/couch_document_validations'
+#require 'spec/rails/matchers/couch_document_validations'
 
 config.gem 'couchrest'
 config.gem 'validatable'
--- lib/autotest/discover.rb
+++ lib/autotest/discover.rb
@@ -1,5 +1,5 @@
-$:.push(File.join(File.dirname(__FILE__), %w[.. .. rspec]))  
-
-Autotest.add_discovery do  
-  "rspec" 
-end
+# $:.push(File.join(File.dirname(__FILE__), %w[.. .. rspec]))  
+# 
+# Autotest.add_discovery do  
+#   "rspec" 
+# end
